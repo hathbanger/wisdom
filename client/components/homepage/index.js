abe9cf58c3c560e163d993e4c7117cb3f06b9c21
hathbanger <hathbanger.ah@gmail.com>
2017-02-27 15:04:16 -0700

    added reactstrap

--- client/components/homepage/index.js
+++ client/components/homepage/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 import { Link } from 'react-router';
 import { example, p, link } from './styles';
+import { Button } from 'reactstrap';
 
 export default class Homepage extends Component {
   /*eslint-disable */
@@ -25,6 +26,7 @@ export default class Homepage extends Component {
         Hot Reloadable <br />
         Golang + React + Redux + Css-Modules
         <br />Isomorphic Starter Kit</h1>
+      <Button>Button!</Button>
       <br />
       <p className={p}>
         Please take a look at <Link className={link} to='/docs'>usage</Link> page.
